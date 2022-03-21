eb48e7b79cf6533a465d80bdc0ef2e10f510c782
Martin Dörfelt <martin.d@andix.de>
2022-03-21 18:13:08 +0100

    add first more complex test case
    Use express instead of vanilla js for convenience.

--- __tests__/test_server.js
+++ __tests__/test_server.js
@@ -1,48 +1,65 @@
-const http = require("http");
+const express = require("express");
+const fetch = require("node-fetch");
 
 const browserWrapper = require("../scripts/browser_wrapper");
 
 const { processMail } = require("../scripts/process_mail");
 
+// Replace the javascript fetch with nodejs fetch.
+global.fetch = jest.fn(fetch);
+
 // Mock all browser access, since we aren't in the frontend.
 jest.mock("../scripts/browser_wrapper");
 
+// TODO: How to put this in a class and mock only the member functions?
+let localStorage;
+
+browserWrapper.getSetting.mockImplementation(async (name) => {
+  // console.log(localStorage);
+  return localStorage[name];
+});
+browserWrapper.getSettings.mockImplementation(async (names) => {
+  let settings = {};
+  for (key of names) {
+    settings[key] = localStorage[key];
+  }
+  return settings;
+});
+browserWrapper.setSetting.mockImplementation(async (name, value) => {
+  localStorage[name] = value;
+});
+
 // Simple test server. Will receive the request that should go to Joplin.
-let server = null;
+let app = express();
+let server;
+let requests;
+
+const getJsonBody = async (request) => {
+  // https://nodejs.dev/learn/get-http-request-body-data-using-nodejs
+  let body = "";
+  request.on("data", (data) => {
+    body += data;
+  });
+  request.on("end", () => {
+    return JSON.parse(body);
+  });
+};
+
+console.log = jest.fn();
 
 beforeAll(() => {
   // https://nodejs.org/api/http.html#httpcreateserveroptions-requestlistener
-  server = http.createServer().listen(41142);
+  server = app.listen(41142);
+  app.use(express.json()); // allow easy access of request.body
 });
 
-afterAll((done) => {
-  server.close(() => {
-    done();
-  });
+beforeEach(() => {
+  localStorage = {};
+  requests = [];
 });
 
-describe("server", () => {
-  test("is working", (done) => {
-    const requestListener = function (req, res) {
-      expect(req.method).toBe("GET");
-      expect(req.url).toBe("/");
-
-      res.setHeader("Content-Type", "application/json");
-      res.writeHead(200);
-      res.end(`{"message": "Server working"}`);
-    };
-
-    server.on("request", requestListener);
-
-    http.get({ host: "127.0.0.1", port: 41142 }, function (res) {
-      expect(res.statusCode).toBe(200);
-      expect(res.headers["content-type"]).toBe("application/json");
-      // Parsing the message body is too complex for the simple test.
-
-      // https://stackoverflow.com/a/71247416/7410886
-      done();
-    });
-  });
+afterAll(() => {
+  server.close();
 });
 
 describe("process mail", () => {
@@ -67,7 +84,66 @@ describe("process mail", () => {
   });
 
   test("setting html, html body available", async () => {
+    // TODO: test all permutations, i. e.:
+    // - format preference: HTML/plain
+    // - HTML available: y/n
+    // - Plain available: y/n
+
+    localStorage = {
+      joplinNoteParentFolder: "arbitrary folder",
+      joplinScheme: "http",
+      joplinHost: "127.0.0.1",
+      joplinPort: 41142,
+      joplinToken: "xyz",
+    };
+    const subject = "test subject";
+    const author = "test author";
+    const body = "test body html";
+
+    // TODO: How to flexibly assign and delete routes/middlewares?
+    // https://stackoverflow.com/a/28369539/7410886
+    app.use((req, res, next) => {
+      requests.push(req);
+      res.status(200).send(JSON.stringify({}));
+    });
+
     // TODO: test this and other permutations
+    browserWrapper.getMail.mockResolvedValue({
+      parts: [
+        {
+          contentType: "text/html",
+          body: body,
+          parts: [],
+        },
+        {
+          contentType: "text/plain",
+          body: "",
+          parts: [],
+        },
+      ],
+    });
+
+    const result = await processMail({
+      id: 0,
+      subject: subject,
+      author: author,
+    });
+
+    expect(result).toBe(null);
+    expect(requests.length).toBe(1);
+    expect(requests[0].method).toBe("POST");
+    expect(requests[0].url).toBe(
+      `/notes?fields=id,body&token=${localStorage.joplinToken}`
+    );
+    expect(requests[0].body).toEqual({
+      body_html: body,
+      parent_id: localStorage.joplinNoteParentFolder,
+      title: `${subject} from ${author}`,
+    });
+
+    // Finally check the console output.
+    expect(console.log.mock.calls.length).toBe(1);
+    expect(console.log.mock.calls[0][0]).toBe("Sending data in HTML format.");
   });
 });
 
